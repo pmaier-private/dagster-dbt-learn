e57a1b12d1438f094b406820c91ccde41886276f
Paul Maier <dr.paul.maier@gmail.com>
2026-04-08 15:01:49 +0200

    renamed and explict col select

--- dbt_lab/models/example/marts/voicecalls/fct_events.sql
+++ dbt_lab/models/example/marts/voicecalls/fct_events.sql
@@ -1,6 +1,12 @@
 
 -- Use the `ref` function to select from other models
 
-select *
+select
+    timestamp,
+    call_id,
+    name,
+    event_type,
+    transcript,
+    company
 from {{ ref('stg_deduplicate_raw') }}
 where event_type = 'call_initiated'
